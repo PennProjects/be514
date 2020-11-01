b52030f383c3e0171902fae5ef44e3a162f2935d
Jal Panchal <jlpnchl@gmail.com>
2020-10-31 22:12:25 -0400

    Pot to servo translation

--- Arduino/1d_potentiometer_servo/1d_potentiometer_servo.ino
+++ Arduino/1d_potentiometer_servo/1d_potentiometer_servo.ino
@@ -1,28 +1,37 @@
 
 //1d Portemtiometer controlled servo
+//In this code the turn of the potentiometer is used to control the angle of a servo
 
+#include <Servo.h>
 
-// define switch and led pins
+// define potentiometer and servo pins
 int pot_pin = 0;
 int pot_value = 0;
+int servo_pin = 9;
 
+//Create Servo object
+Servo servo_motor;
 
 
 // the setup routine runs once when you press reset:
 void setup() {
   
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
- 
+  //Set the servo control pin
+  servo_motor.attach(servo_pin);
 
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
 
-  //Read value fro Potentiometer
+  //Read value from Potentiometer
   pot_value = analogRead(pot_pin);
-  Serial.println(pot_value);
-  delay(1);
+  
+  //map Pot value from 0-1023 to 0-180 deg
+  pot_value = map(pot_value, 0,1023,0,180);
+
+  //send the values to the servo
+  servo_motor.write(pot_value);
+  delay(10);
 
 }
